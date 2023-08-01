24436383534d7d9349e13e2a3719f86227e21366
Andre Luiz Monteiro da Rocha <andreluiz@MacBook-Air-de-Andre.local>
2023-08-01 06:19:19 -0400

    Arquivo password. Resgate Usuaario

--- inspector/password.py
+++ inspector/password.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+usuario = ''
+
 def check_password():
     """Returns `True` if the user had a correct password.
 
@@ -8,30 +10,40 @@ def check_password():
     """
 
     def password_entered():
+        global usuario
         """Checks whether a password entered by the user is correct."""
         if (
             st.session_state["username"] in st.secrets["passwords"]
-            and st.session_state["password"]
-            == st.secrets["passwords"][st.session_state["username"]]
+            and st.session_state["password"] == st.secrets["passwords"][st.session_state["username"]]
         ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store username + password
-            del st.session_state["username"]
+            st.session_state["username"]
+            # del st.session_state["password"]  # don't store username + password
+            # del st.session_state["username"]
+            del st.session_state["password"]  # don't store password
+            usuario = st.session_state["username"]
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
         # First run, show inputs for username + password.
-        st.text_input("Username", on_change=password_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+        st.text_input("Username", 
+                      on_change=password_entered, 
+                      key="username")
+        st.text_input("Password", 
+                      type="password", 
+                      on_change=password_entered, 
+                      key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
-        st.text_input("Username", on_change=password_entered, key="username")
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+        st.text_input("Username", 
+                      on_change=password_entered, 
+                      key="username")
+        st.text_input("Password", 
+                      type="password", 
+                      on_change=password_entered, key="password"
         )
         st.error("😕 Usuário não conhecido ou password incorreto.")
         return False
